2a282cbcbd5b9212ccb91c8cb550503ac2a1a1a9
maxiluckiesgin <luckiesmaxi6@gmail.com>
2018-12-13 17:28:44 +0700

    holly molly this is a mess

--- src/main/java/spark/KafkaSpark.java
+++ src/main/java/spark/KafkaSpark.java
@@ -1,27 +1,22 @@
 package spark;
 
-import com.arangodb.spark.ArangoSpark;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.VoidFunction2;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SQLContext;
-import org.apache.spark.sql.functions;
+import org.apache.spark.sql.*;
+import org.apache.spark.sql.streaming.DataStreamWriter;
 import org.apache.spark.sql.streaming.StreamingQuery;
 import org.apache.spark.sql.streaming.StreamingQueryException;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructType;
-import org.apache.spark.streaming.Duration;
-import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.json.JSONObject;
 
-import java.util.Collection;
-import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
 
 class KafkaSpark {
-    KafkaSpark(){
+    KafkaSpark() {
 
     }
 
@@ -34,47 +29,78 @@ class KafkaSpark {
                 .set("arangodb.password", "arangodebe");
         JavaSparkContext sparkContext = new JavaSparkContext(conf);
 
-        JavaStreamingContext streamingContext = new JavaStreamingContext(sparkContext, new Duration(1000));
-
-        Collection<String> topics = Collections.singletonList("sparkStream");
-
-
-        ObjectMapper objectMapper = new ObjectMapper();
-
         SQLContext sqlContext = new SQLContext(sparkContext);
 
         Dataset<Row> df = sqlContext.readStream()
                 .format("kafka")
                 .option("kafka.bootstrap.servers", "localhost:9092")
-                .option("startingOffsets","earliest")
+                .option("startingOffsets", "latest")
                 .option("subscribe", "sparkStream")
                 .load();
 
 
-        StructType book = new StructType()
-                .add("title", DataTypes.StringType)
-                .add("author", DataTypes.StringType)
-                .add("publisher", DataTypes.StringType);
+        StructType book = new StructType();
 
         Dataset<Row> result = df.selectExpr("CAST(value AS STRING)").select("value");
+
+
         Dataset<Row> toBook = result.select(functions.from_json(
-                result.col("value"),book)
+                result.col("value"), book)
                 .as("book")
         );
 
         //select column
-        Dataset<Row> filteredBook = toBook.selectExpr("book.title","book.author");
+        Dataset<Row> filteredBook = toBook.selectExpr("*");
 
 
-        StreamingQuery squery = filteredBook.writeStream()
+        DataStreamWriter<Row> bookQuery = filteredBook.writeStream()
                 .foreachBatch((VoidFunction2<Dataset<Row>, Long>) (rowDataset, aLong) -> {
-                    rowDataset.write()
-                            .mode("append")
-                            .parquet("hdfs://localhost/datastore/blablabla-1.parquet");
-                    ArangoSpark.save(rowDataset, "sparkDummy");
+//                    squery.stop();
+                    if(!rowDataset.isEmpty()){
+                        System.out.println("using latest");
+                        rowDataset.show();
+//                        rowDataset.write()
+//                                .mode("append")
+//                                .parquet("hdfs://localhost/datastore/blablabla-1.parquet");
+//                        ArangoSpark.save(rowDataset, "sparkDummy");
+                    }
+
+                });
+
+
+        StreamingQuery squery = result.writeStream()
+                .foreachBatch((VoidFunction2<Dataset<Row>, Long>) (rowDataset, aLong) -> {
+                    if (!rowDataset.isEmpty()) {
+                        List<String> listOne = rowDataset.as(Encoders.STRING()).collectAsList();
+
+                        System.out.println(listOne.get(0));
+
+                        JSONObject jsonObject = new JSONObject(listOne.get(0).trim());
+
+                        Iterator<String> keys = jsonObject.keys();
+
+                        while (keys.hasNext()) {
+                            String key = keys.next();
+                            book.add(key, DataTypes.StringType);
+                        }
+
+                        bookQuery.start().awaitTermination();
+
+                    }
+
+//
                 })
                 .start();
+
+
+
+
         squery.awaitTermination();
+
+
+
+
+
 
     }
 
