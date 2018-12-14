d37a0dd9defc65a47742e0336e68f70c74c5685c
maxiluckiesgin <luckiesmaxi6@gmail.com>
2018-12-14 13:29:36 +0700

    non working using multiple write stream

--- src/main/java/spark/KafkaSpark.java
+++ src/main/java/spark/KafkaSpark.java
@@ -4,13 +4,14 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.VoidFunction2;
 import org.apache.spark.sql.*;
-import org.apache.spark.sql.streaming.DataStreamWriter;
 import org.apache.spark.sql.streaming.StreamingQuery;
 import org.apache.spark.sql.streaming.StreamingQueryException;
 import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -39,38 +40,17 @@ class KafkaSpark {
                 .load();
 
 
-        StructType book = new StructType();
+        ArrayList<StructField> sf = new ArrayList<StructField>();
+
 
         Dataset<Row> result = df.selectExpr("CAST(value AS STRING)").select("value");
 
-
-        Dataset<Row> toBook = result.select(functions.from_json(
-                result.col("value"), book)
-                .as("book")
-        );
-
-        //select column
-        Dataset<Row> filteredBook = toBook.selectExpr("*");
-
-
-        DataStreamWriter<Row> bookQuery = filteredBook.writeStream()
-                .foreachBatch((VoidFunction2<Dataset<Row>, Long>) (rowDataset, aLong) -> {
-//                    squery.stop();
-                    if(!rowDataset.isEmpty()){
-                        System.out.println("using latest");
-                        rowDataset.show();
-//                        rowDataset.write()
-//                                .mode("append")
-//                                .parquet("hdfs://localhost/datastore/blablabla-1.parquet");
-//                        ArangoSpark.save(rowDataset, "sparkDummy");
-                    }
-
-                });
 
 
         StreamingQuery squery = result.writeStream()
                 .foreachBatch((VoidFunction2<Dataset<Row>, Long>) (rowDataset, aLong) -> {
                     if (!rowDataset.isEmpty()) {
+
                         List<String> listOne = rowDataset.as(Encoders.STRING()).collectAsList();
 
                         System.out.println(listOne.get(0));
@@ -81,10 +61,35 @@ class KafkaSpark {
 
                         while (keys.hasNext()) {
                             String key = keys.next();
-                            book.add(key, DataTypes.StringType);
+                             sf.add(new StructField(key, DataTypes.StringType, true, null));
                         }
 
-                        bookQuery.start().awaitTermination();
+                        StructType book = new StructType(sf.toArray(new StructField[0]));
+
+                        Dataset<Row> result2 = df.selectExpr("CAST(value AS STRING)").select("value");
+
+                        Dataset<Row> toBook = result2.select(functions.from_json(
+                                result2.col("value"), book)
+                                .as("book")
+                        );
+
+                        //select column
+                        Dataset<Row> filteredBook = toBook.selectExpr("book.*");
+
+                        StreamingQuery bookQuery = filteredBook.writeStream()
+                                .foreachBatch((VoidFunction2<Dataset<Row>, Long>) (bookDataset, bookLong) -> {
+                                        System.out.println("using latest");
+                                        bookDataset.show(false);
+//                        rowDataset.write()
+//                                .mode("append")
+//                                .parquet("hdfs://localhost/datastore/blablabla-1.parquet");
+//                        ArangoSpark.save(rowDataset, "sparkDummy");
+
+
+                                })
+                                .start();
+
+                        bookQuery.awaitTermination();
 
                     }
 
@@ -92,10 +97,16 @@ class KafkaSpark {
                 })
                 .start();
 
+//
 
 
 
-        squery.awaitTermination();
+
+
+
+
+
+        sqlContext.streams().awaitAnyTermination();
 
 
 
